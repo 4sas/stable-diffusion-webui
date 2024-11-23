37917365709640b04830f430df60fe73d5819a5d
TRC_Unity <trc.unity@gmail.com>
2024-11-23 15:46:57 +0900

    fix error

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,11 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\k-sakanaka\AppData\Local\mise\installs\python\3.10.6\python.exe
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
+
+@REM エラー対策: RuntimeError: Windows not yet supported for torch.compile
+set TORCHDYNAMO_DISABLE=1
 
 call webui.bat
